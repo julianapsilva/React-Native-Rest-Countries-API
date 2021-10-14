a1a39dcfd8e65a5e57aeffb7f1c1332b252df3a8
julianapsilva <julianadepaula001@gmail.com>
2021-10-14 20:33:37 -0300

    header

---
+++ src/components/Header/index.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+
+class Header extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Where in the world?</Text>
+        <TouchableOpacity>
+          <Text>Dark Mode</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    elevation: 5,
+    padding: 20,
+  },
+  title: {
+    fontWeight: 'bold',
+  },
+});
+
+export default Header;
