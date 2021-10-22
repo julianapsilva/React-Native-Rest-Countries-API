74cd4f97d0bce06166194167865ecc63a92c8570
julianapsilva <julianadepaula001@gmail.com>
2021-10-21 21:06:06 -0300

    implementa funções de filtro

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,16 +1,54 @@
 //import liraries
 import React, {Component} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Keyboard} from 'react-native';
 import Header from '../../components/Header';
+import Filters from '../../components/Filters';
 import CountriesList from '../../components/CountriesList';
+import api from '../../services/api';
 
 // create a component
 class Home extends Component {
+  async componentDidMount() {
+    api.get('v2/all').then(response => {
+      this.setState({
+        allCountries: response.data,
+        countries: response.data,
+      });
+    });
+  }
+
+  state = {
+    allCountries: [],
+    countries: [],
+  };
+
+  handleFilterSelect = filterValue => {
+    console.log('fil', filterValue);
+    const filterBy = this.state.allCountries.filter(
+      country => country.region === filterValue,
+    );
+    this.setState({countries: filterBy});
+  };
+
+  handleFilterSearch = filterValue => {
+    const filterBy = this.state.allCountries.filter(({name}) => {
+      return (
+        name.toString().toLowerCase().indexOf(filterValue.toLowerCase()) >= 0
+      );
+    });
+    this.setState({countries: filterBy});
+    setInterval(() => Keyboard.dismiss(), 5000);
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <Header />
-        <CountriesList />
+        <Filters
+          updateFilterSelect={this.handleFilterSelect}
+          updateFilterSearch={this.handleFilterSearch}
+        />
+        <CountriesList countries={this.state.countries} />
       </View>
     );
   }
