4f55e8a0c839ead411e99714d3d35800f9c5abe3
julianapsilva <julianadepaula001@gmail.com>
2021-10-23 20:16:55 -0300

    adiciona icon moon no header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,31 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {ThemeContext} from '../../../App';
+import {useTheme} from '@react-navigation/native';
+import {light, dark} from '../../themes';
+import Icon from 'react-native-vector-icons/Feather';
 
-class Header extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Where in the world?</Text>
-        <TouchableOpacity>
-          <Text>Dark Mode</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
+function Header() {
+  const {setTheme, theme} = React.useContext(ThemeContext);
+  const {colors} = useTheme();
+
+  return (
+    <View style={[{backgroundColor: colors.elements}, styles.container]}>
+      <Text style={[styles.title, {color: colors.text}]}>
+        Where in the world?
+      </Text>
+      <TouchableOpacity
+        style={styles.toggleTheme}
+        onPress={() => setTheme(theme === light ? dark : light)}>
+        <Icon
+          name="moon"
+          size={20}
+          style={[styles.moon, {color: colors.text}]}
+        />
+        <Text style={{color: colors.text}}>Dark Mode</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -21,10 +35,15 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     elevation: 4,
     padding: 20,
-    backgroundColor: '#fff',
   },
   title: {
     fontWeight: 'bold',
+  },
+  toggleTheme: {
+    flexDirection: 'row',
+  },
+  moon: {
+    marginRight: 7,
   },
 });
 
