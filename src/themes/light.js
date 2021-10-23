3eebcaf6ba98aa6c719e1acc9abb70eb83b2118f
julianapsilva <julianadepaula001@gmail.com>
2021-10-23 20:17:28 -0300

    arquivos com configurações cores theme switcher

---
+++ src/themes/light.js
@@ -0,0 +1,9 @@
+export default {
+  dark: false,
+  colors: {
+    background: 'hsl(0, 0%, 97%)',
+    elements: 'hsl(0, 0%, 100%)',
+    text: 'hsl(200, 15%, 8%)',
+    input: 'hsl(0, 0%, 52%)',
+  },
+};
