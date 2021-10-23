b39b60d6f9a3e262ac7b510b66e21deef7382b7f
julianapsilva <julianadepaula001@gmail.com>
2021-10-23 20:16:28 -0300

    adapta filtros a dark theme

--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -3,21 +3,28 @@ import React, {Component} from 'react';
 import {View, TextInput, StyleSheet} from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 import Icon from 'react-native-vector-icons/Feather';
+import {useTheme} from '@react-navigation/native';
 
 // create a component
-class Filters extends Component {
+class FiltersClass extends Component {
   state = {
     region: '',
   };
   render() {
+    const {elements, text, input} = this.props.colors;
     return (
       <View style={styles.container}>
         <TextInput
-          style={styles.input}
+          style={[
+            styles.input,
+            styles.boxShadow,
+            {backgroundColor: elements, color: text},
+          ]}
           onChangeText={value => this.props.updateFilterSearch(value)}
           placeholder="Search for a country..."
+          placeholderTextColor={input}
         />
-        <Icon name="search" size={20} style={styles.icon} />
+        <Icon name="search" size={17} style={[styles.icon, {color: input}]} />
 
         <Picker
           selectedValue={this.state.region}
@@ -26,7 +33,11 @@ class Filters extends Component {
             this.props.updateFilterSelect(value);
           }}
           mode="dropdown" // Android only
-          style={styles.picker}>
+          style={[
+            styles.picker,
+            styles.boxShadow,
+            {backgroundColor: elements, color: text},
+          ]}>
           <Picker.Item label="Filter by Region" value="Unknown" />
           <Picker.Item label="Africa" value="Africa" />
           <Picker.Item label="Americas" value="Americas" />
@@ -37,6 +48,11 @@ class Filters extends Component {
       </View>
     );
   }
+}
+
+function Filters(props) {
+  const {colors} = useTheme();
+  return <FiltersClass {...props} colors={colors} />;
 }
 
 // define your styles
@@ -53,11 +69,9 @@ const styles = StyleSheet.create({
     width: 200,
     padding: 10,
     borderWidth: 1,
-    backgroundColor: '#fff',
     alignSelf: 'flex-start',
   },
   input: {
-    backgroundColor: '#fff',
     width: 360,
     position: 'relative',
     paddingLeft: 60,
@@ -66,8 +80,17 @@ const styles = StyleSheet.create({
   },
   icon: {
     position: 'absolute',
-    top: 37,
-    left: 17,
+    top: 39,
+    left: 22,
+    elevation: 2,
+  },
+  boxShadow: {
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 1},
+    shadowOpacity: 0.9,
+    shadowRadius: 1,
+    elevation: 1,
+    borderColor: '#000',
   },
 });
 
