d3e831715040860ec3b8e5d08c17069df312c795
julianapsilva <julianadepaula001@gmail.com>
2021-10-23 20:14:52 -0300

    adiciona dark theme

--- App.js
+++ App.js
@@ -1,26 +1,33 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Home from './src/views/Home';
 import Country from './src/views/Country';
+import {light, dark} from './src/themes';
+export const ThemeContext = React.createContext();
 
 const Stack = createStackNavigator();
 
-class App extends Component {
-  render() {
-    return (
-      <NavigationContainer>
+export default function App() {
+  const [theme, setTheme] = useState(light);
+
+  const themeData = {theme, setTheme};
+  return (
+    <ThemeContext.Provider value={themeData}>
+      <NavigationContainer theme={theme === light ? dark : light}>
         <Stack.Navigator>
           <Stack.Screen
             name="Home"
             component={Home}
             options={{headerShown: false}}
           />
-          <Stack.Screen name="Country" component={Country} />
+          <Stack.Screen
+            name="Country"
+            component={Country}
+            options={{headerShown: false}}
+          />
         </Stack.Navigator>
       </NavigationContainer>
-    );
-  }
+    </ThemeContext.Provider>
+  );
 }
-
-export default App;
