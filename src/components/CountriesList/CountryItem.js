88b57e26df54cf4431b9ceabc05a64791d6dc294
julianapsilva <julianadepaula001@gmail.com>
2021-10-23 20:15:44 -0300

    muda cores

--- src/components/CountriesList/CountryItem.js
+++ src/components/CountriesList/CountryItem.js
@@ -2,6 +2,7 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
+import {useTheme} from '@react-navigation/native';
 import {SvgUri} from 'react-native-svg';
 
 // create a component
@@ -9,28 +10,30 @@ class CountryItemClass extends Component {
   render() {
     const {name, population, capital, region, flag} = this.props.data;
     const {navigation} = this.props;
+    const {text, elements} = this.props.colors;
     const knowMore = () => {
       navigation.navigate('Country', {data: this.props.data});
     };
     const format = pop => {
       return String(pop).replace(/(.)(?=(\d{3})+$)/g, '$1,');
     };
-
     return (
-      <TouchableOpacity style={styles.CountryItem} onPress={knowMore}>
+      <TouchableOpacity
+        style={[styles.CountryItem, {backgroundColor: elements, color: text}]}
+        onPress={knowMore}>
         <SvgUri width={298} height={250} uri={flag} style={styles.flag} />
         <View style={styles.info}>
-          <Text style={styles.name}>{name}</Text>
-          <Text style={styles.countryInfo}>
-            <Text style={{fontWeight: 'bold'}}>Population: </Text>
+          <Text style={[styles.name, {color: text}]}>{name}</Text>
+          <Text style={[styles.countryInfo, {color: text}]}>
+            <Text style={styles.bold}>Population: </Text>
             <Text>{format(population)}</Text>
           </Text>
-          <Text style={styles.countryInfo}>
-            <Text style={{fontWeight: 'bold'}}>Region: </Text>
+          <Text style={[styles.countryInfo, {color: text}]}>
+            <Text style={styles.bold}>Region: </Text>
             <Text>{region}</Text>
           </Text>
-          <Text style={styles.countryInfo}>
-            <Text style={{fontWeight: 'bold'}}>Capital: </Text>
+          <Text style={[styles.countryInfo, {color: text}]}>
+            <Text style={styles.bold}>Capital: </Text>
             <Text>{capital}</Text>
           </Text>
         </View>
@@ -41,7 +44,10 @@ class CountryItemClass extends Component {
 
 export default function CountryItem({data}) {
   const navigation = useNavigation();
-  return <CountryItemClass navigation={navigation} data={data} />;
+  const {colors} = useTheme();
+  return (
+    <CountryItemClass navigation={navigation} colors={colors} data={data} />
+  );
 }
 
 // define your styles
@@ -51,12 +57,11 @@ const styles = StyleSheet.create({
     width: 300,
     height: 420,
     shadowColor: '#000',
-    shadowOffset: {width: 0, height: 1},
+    shadowOffset: {width: 10, height: 10},
     shadowOpacity: 0.9,
     shadowRadius: 1,
     elevation: 1,
     borderColor: '#000',
-    backgroundColor: '#fff',
   },
   name: {
     fontWeight: 'bold',
@@ -73,5 +78,8 @@ const styles = StyleSheet.create({
   },
   flag: {
     marginTop: -30,
+  },
+  bold: {
+    fontWeight: 'bold',
   },
 });
