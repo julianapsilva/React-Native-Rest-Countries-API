38881cc5ea307ba7bbebb30d11bb31139edd0add
julianapsilva <julianadepaula001@gmail.com>
2021-10-24 15:06:53 -0300

    add async storage

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Home from './src/views/Home';
 import Country from './src/views/Country';
 import {light, dark} from './src/themes';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 export const ThemeContext = React.createContext();
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  useEffect(() => {
+    (async () => {
+      try {
+        const value = await AsyncStorage.getItem('theme');
+        if (value === 'dark') {
+          setTheme(dark);
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    })();
+  }, []);
   const [theme, setTheme] = useState(light);
-
   const themeData = {theme, setTheme};
   return (
     <ThemeContext.Provider value={themeData}>
-      <NavigationContainer theme={theme === light ? dark : light}>
+      <NavigationContainer theme={theme}>
         <Stack.Navigator>
           <Stack.Screen
             name="Home"
